3af286fc4a25551d578f6c4cd652067cfe7fca28
molly12345molly <809612945@qq.com>
2020-10-11 14:35:46 +0800

    流失思想skip方法之类的引用

--- StreamDemo/src/Demo.java
+++ StreamDemo/src/Demo.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Demo {
     public static void main(String[] args) {
@@ -16,6 +17,32 @@ public class Demo {
         //两次过滤，一次输出
         list.stream().filter(name->name.startsWith("张")).filter(name->name.length()>3).forEach(name-> System.out.println(name));
         //流思想概述----拼接流式模型，按照生产线生产物品，例如生产饮料---放瓶子，洗瓶子，装饮料，封口，装箱
+
+        //所有的collection集合都可以通过Stream获取流  map方法
+        Stream<String> stream=Stream.of("1","2","3","4");
+        Stream<Integer> stream1=stream.map((String s)->{
+            return Integer.parseInt(s);
+        });
+        stream1.forEach(i-> System.out.println(i));
+
+        String[] arr={"美羊羊","喜洋洋","红太狼"};
+        Stream<String> stream2=Stream.of(arr);
+        Stream<String> stream3=stream2.limit(1);
+        //遍历流
+        //stream3.forEach(name-> System.out.println(name));
+
+        //跳过前几个元素的方法
+        String[] arr1={"美羊羊","喜洋洋","红太狼"};
+        Stream<String> stream4=Stream.of(arr1);
+        Stream<String> stream5=stream4.skip(1);
+        //stream5.forEach(name-> System.out.println(name));
+
+        Stream<String> concat=Stream.concat(stream3,stream5);
+        concat.forEach(name-> System.out.println(name));
+
+
+
+
 
 
 
